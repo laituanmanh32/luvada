4ea2d584b56e5966cca3793ceb2703223d949978
Manh Lai <laituanmanh32@gmail.com>
2020-10-16 18:05:05 +0700

    github

--- git-etop/src/github.rs
+++ git-etop/src/github.rs
@@ -1,3 +1,9 @@
+extern crate reqwest;
+use self::reqwest::Client;
+use self::reqwest::header;
+
+const GITHUB_API_URL: &'static str =  "https://api.github.com";
+
 struct GithubAuth {
     username: String,
     name: String,
@@ -5,15 +11,25 @@ struct GithubAuth {
     token: String,
 }
 
-pub async fn login() {
+pub async fn login() -> Result<(), Box<dyn std::error::Error>> {
     println!("Go to https://github.com/settings/tokens and generate token with permissions: [read:user, write:discussion]");
     println!("Enter token: ");
 
-    let mut guess = String::new();
+    let mut githubToken = String::new();
 
     std::io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut githubToken)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let request_url = "https://api.github.com/user";
+    let auth = format!("token {}", githubToken);
+
+    let client = Client::new()
+        .get(request_url)
+        .header(header::AUTHORIZATION, "token a8e92a149b4f0f931fd1b267766a52403597a4fe")
+        .send()
+        .await;
+    println!("RESPONSE {} {} \n{:?}",request_url, auth, client);
+
+    Ok(())
 }
--- git-etop/src/main.rs
+++ git-etop/src/main.rs
@@ -1,16 +1,7 @@
-use std::collections::HashMap;
-
 mod github;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let resp = reqwest::get("https://httpbin.org/ip")
-    //     .await?
-    //     .json::<HashMap<String, String>>()
-    //     .await?;
-    // println!("{:#?}", resp);
-    // Ok(())
-
     github::login().await;
     Ok(())
 }
